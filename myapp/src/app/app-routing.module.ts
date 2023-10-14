c0139187f5c48477eadcc8da311336d2cb507347
Rohit Kumar Anchal <105239963+rohitanchal@users.noreply.github.com>
2023-10-14 11:27:33 +0530

    add signup module

--- myapp/src/app/app-routing.module.ts
+++ myapp/src/app/app-routing.module.ts
@@ -2,8 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
+import { SignupComponent } from './signup/signup.component';
+
 const routes: Routes = [
-  {path: 'home', component: HomeComponent}
+  {path: 'home', component: HomeComponent},
+  {path: 'signup' , component: SignupComponent}
 ];
 
 @NgModule({
--- myapp/src/app/app.module.ts
+++ myapp/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
