da38d0f005f9b6beafa9cfb5c1e33114875f7763
Rohit Kumar Anchal <105239963+rohitanchal@users.noreply.github.com>
2023-10-15 23:59:54 +0530

    add login

--- myapp/src/app/app.module.ts
+++ myapp/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    SignupComponent
+    SignupComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
